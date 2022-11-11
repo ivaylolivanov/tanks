f5a8c0dac445db56f2775359d7d98f21247d9bf8
Ivaylo <ivaylolivanov95@gmail.com>
2022-11-11 13:57:11 +0200

    build.bat: Use 'clang' instead 'cl'
    Signed-off-by: Ivaylo <ivaylolivanov95@gmail.com>

--- tools/windows/build.bat
+++ tools/windows/build.bat
@@ -3,15 +3,22 @@
 set SOURCE_DIR=%WORKING_DIR_ROOT%\src
 set BUILD_DIR=%WORKING_DIR_ROOT%\build
 
-set COMMON_COMPILER_FLAGS=-nologo -Od -Oi -WX -W4 -wd4201 -wd4100 -wd4189 -wd4505 -FC -Z7
-set COMMON_LINKER_FLAGS= -incremental:no -opt:ref user32.lib gdi32.lib winmm.lib
+rem NOTE:
+rem These example flags are left so we can have a reference for
+rem what will in the future be necessary.
+rem
+rem set COMMON_COMPILER_FLAGS=-nologo -Od -Oi -WX -W4 -wd4201 -wd4100 -wd4189 -wd4505 -FC -Z7
+rem set COMMON_LINKER_FLAGS= -incremental:no -opt:ref user32.lib gdi32.lib winmm.lib
+set COMMON_COMPILER_FLAGS=-g -gcodeview
+set COMMON_LINKER_FLAGS=-l user32.lib -l gdi32.lib -l winmm.lib
 
 mkdir %BUILD_DIR%
 pushd %BUILD_DIR%
 
 del %BUILD_DIR%\*.pdb > NUL 2> NUL
 
-cl %COMMON_COMPILER_FLAGS% %SOURCE_DIR%\platform-layer\windows\win-32.cpp^
-  /link %COMMON_LINKER_FLAGS%
+clang %COMMON_COMPILER_FLAGS% %COMMON_LINKER_FLAGS%^
+      %SOURCE_DIR%\platform-layer\windows\win-32.cpp^
+      -o win-32.exe -v
 
 popd
