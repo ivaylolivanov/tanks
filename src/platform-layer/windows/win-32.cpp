ec3b7b943fb28996cd3ab648ab12525f2427bd1e
Ivaylo Ivanov <ivaylo@beastburst.com>
2023-08-08 09:02:50 +0300

    win-32.cpp: Refactor, make code standard same

--- src/platform-layer/windows/win-32.cpp
+++ src/platform-layer/windows/win-32.cpp
@@ -5,6 +5,7 @@
 #include <windows.h>
 #include <wchar.h>
 #include <cinttypes>
+#include <winuser.h>
 
 // Better send the intetion of the static keyword to the other developers
 #define Internal       static
@@ -13,42 +14,154 @@
 
 #define Pi32 3.14159265359f
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 typedef int8_t int8;
 typedef int16_t int16;
 typedef int32_t int32;
 typedef int64_t int64;
 typedef int32 bool32;
 
-int WINAPI WinMain(HINSTANCE instance, HINSTANCE prevInstance, LPSTR cmdLine, int cmdCharactersCount)
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
+typedef float real32;
+typedef double real64;
+
+GlobalVariable bool IS_RUNNING = false;
+
+Internal void ProcessPendingMessages()
+{
+    MSG message;
+    while (PeekMessage(&message, 0, 0, 0, PM_REMOVE))
+    {
+        switch (message.message)
+        {
+            case WM_QUIT:
+            {
+                IS_RUNNING = false;
+            } break;
+
+            case WM_SYSKEYUP:
+            case WM_SYSKEYDOWN:
+            case WM_KEYUP:
+            case WM_KEYDOWN:
+            {
+                uint32_t vk_code = (uint32_t)message.wParam;
+                bool was_down = ((message.lParam & (1 << 30)) != 0);
+                bool is_down = ((message.lParam & (1 << 31)) == 0);
+
+                bool alt_was_down = (message.lParam & (1 << 29));
+                if (was_down == is_down) continue;
+
+                switch (vk_code)
+                {
+                    case 'W':
+                    {
+                        OutputDebugStringA("You have pressed W/w.\n");
+                    } break;
+
+                    case 'A':
+                    {
+                        OutputDebugStringA("You have pressed A/a.\n");
+                    } break;
+
+                    case 'S':
+                    {
+                        OutputDebugStringA("You have pressed S/s.\n");
+                    } break;
+
+                    case 'D':
+                    {
+                        OutputDebugStringA("You have pressed D/d.\n");
+                    } break;
+
+                    case VK_SPACE:
+                    {
+                        OutputDebugStringA("You have pressed spacebar.\n");
+                    } break;
+
+                    case VK_ESCAPE:
+                    {
+                        OutputDebugStringA("You have pressed ESC key.\n");
+                        IS_RUNNING = false;
+                    } break;
+
+                    case VK_RETURN:
+                    {
+                        OutputDebugStringA("You have pressed RETURN key.\n");
+                    } break;
+
+                    case VK_F4:
+                    {
+                        // TODO: Pass too?
+                        if (alt_was_down)
+                        {
+                            IS_RUNNING = false;
+                            OutputDebugStringA("You have pressed Alt + F4 key.\n");
+                        }
+                    } break;
+                }
+            }
+            default:
+            {
+                TranslateMessage(&message);
+                DispatchMessageA(&message);
+            } break;
+        }
+    }
+}
+
+LRESULT CALLBACK WindowProc(HWND window, UINT message, WPARAM wparam, LPARAM lparam)
+{
+    switch (message)
+    {
+        case WM_DESTROY:
+        {
+            IS_RUNNING = false;
+            PostQuitMessage(0);
+            return 0;
+        }
+
+        case WM_PAINT:
+        {
+            PAINTSTRUCT paint_struct;
+            HDC device_context = BeginPaint(window, &paint_struct);
+
+            // All painting occurs here, between BeginPaint and EndPaint.
+
+            FillRect(device_context, &paint_struct.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+
+            EndPaint( window, &paint_struct);
+        }
+        return 0;
+
+    }
+    return DefWindowProc(window, message, wparam, lparam);
+}
+
+int WINAPI WinMain(HINSTANCE instance, HINSTANCE prev_instance, LPSTR command_line, int command_line_characters_count)
 {
     IS_RUNNING = false;
 
     // Register the window class.
     const wchar_t CLASS_NAME[] = L"Sample Window Class";
 
-    WNDCLASS wc = { };
+    WNDCLASS window_class = { };
 
-    wc.lpfnWndProc = WindowProc;
-    wc.hInstance = instance;
-    wc.lpszClassName = CLASS_NAME;
-typedef uint8_t uint8;
-typedef uint16_t uint16;
-typedef uint32_t uint32;
-typedef uint64_t uint64;
+    window_class.lpfnWndProc = WindowProc;
+    window_class.hInstance = instance;
+    window_class.lpszClassName = CLASS_NAME;
 
-    RegisterClass(&wc);
-typedef float real32;
-typedef double real64;
+    RegisterClass(&window_class);
 
     // Create the window.
 
-    HWND hwnd = CreateWindowEx(
+    HWND window = CreateWindowEx(
         0,                              // Optional window styles.
         CLASS_NAME,                     // Window class
         L"Learn to Program Windows",    // Window text
         WS_OVERLAPPEDWINDOW,            // Window style
-GlobalVariable bool IS_RUNNING = false;
 
         // Size and position
         CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
@@ -59,132 +172,18 @@ GlobalVariable bool IS_RUNNING = false;
         NULL        // Additional application data
     );
 
-    if (hwnd == NULL)
-    {
-        return 0;
-    }
+    if (window == NULL) return 0;
 
-    ShowWindow(hwnd, cmdCharactersCount);
+    ShowWindow(window, command_line_characters_count);
 
     // Run the message loop.
 
     IS_RUNNING = true;
 
-    wchar_t msg[32];
-    swprintf_s(msg, L"Going to the while!");
-    OutputDebugString(msg);
-
     while (IS_RUNNING)
     {
-        MSG msg = { };
-        while (PeekMessage(&msg, hwnd, 0, 0, PM_REMOVE))
-        {
-            if (msg.message == WM_QUIT)
-            {
-                IS_RUNNING = false;
-                return 0;
-            }
-
-            switch (msg.message)
-            {
-            case WM_SYSKEYDOWN:
-            case WM_SYSKEYUP:
-            case WM_KEYDOWN:
-            case WM_KEYUP:
-            {
-                uint32_t vkCode = (uint32_t)msg.wParam;
-                bool wasDown = ((msg.lParam & (1 << 30)) != 0);
-                bool isDown = ((msg.lParam & (1 << 31)) == 0);
-
-                bool altWasDown = (msg.lParam & (1 << 29));
-                if (wasDown != isDown) {
-                    switch (vkCode) {
-                    case 'W': {
-                        OutputDebugStringA("You have pressed W/w.\n");
-                    } break;
-
-                    case 'A': {
-                        OutputDebugStringA("You have pressed A/a.\n");
-                    } break;
-
-                    case 'S': {
-                        OutputDebugStringA("You have pressed S/s.\n");
-                    } break;
-
-                    case 'D': {
-                        OutputDebugStringA("You have pressed D/d.\n");
-                    } break;
-
-                    case VK_SPACE: {
-                        OutputDebugStringA("You have pressed spacebar.\n");
-                    } break;
-
-                    case VK_ESCAPE: {
-                        OutputDebugStringA("You have pressed ESC key.\n");
-                        IS_RUNNING = false;
-                    } break;
-
-                    case VK_RETURN: {
-                        OutputDebugStringA("You have pressed RETURN key.\n");
-
-                    } break;
-
-                    case VK_F4: {
-                        // TODO: Pass too?
-                        if (altWasDown) {
-                            OutputDebugStringA("You have pressed Alt + F4 key.\n");
-                            IS_RUNNING = false;
-                        }
-                    } break;
-                    }
-                }
-            } break;
-            default: {
-                TranslateMessage(&msg);
-                DispatchMessageA(&msg);
-            } break;
-            } break;
-        }
+        ProcessPendingMessages();
     }
-
-    swprintf_s(msg, L"OUT OF THE WHILE\n");
-    OutputDebugString(msg);
 
     return 0;
-}
-
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
-{
-    switch (uMsg)
-    {
-    case WM_DESTROY:
-        IS_RUNNING = false;
-        PostQuitMessage(0);
-        return 0;
-
-    case WM_PAINT:
-    {
-        PAINTSTRUCT ps;
-        HDC hdc = BeginPaint(hwnd, &ps);
-
-        // All painting occurs here, between BeginPaint and EndPaint.
-
-        FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-
-        EndPaint(hwnd, &ps);
-    }
-    // case WM_KEYDOWN :
-    // {
-    //     wchar_t msg[32];
-    //     if (wParam)
-    //     {
-    //         swprintf_s(msg, L"We have pressed 'A'\n");
-    //         OutputDebugString(msg);
-    //     }
-
-    // } break;
-    return 0;
-
-    }
-    return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
