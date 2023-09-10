b76c75739a7b0505c3c9bd17f1640b04891a5e53
Ivaylo <ivaylolivanov95@gmail.com>
2023-09-10 23:59:18 +0300

    Add win-32.h
    Signed-off-by: Ivaylo <ivaylolivanov95@gmail.com>

---
+++ src/platform-layer/windows/win-32.h
@@ -0,0 +1,20 @@
+// Better send the intetion of the static keyword to the other developers
+#define Internal       static
+#define LocalPersist   static
+#define GlobalVariable static
+
+#define Pi32 3.14159265359f
+
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+typedef int32 bool32;
+
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
+typedef float real32;
+typedef double real64;
