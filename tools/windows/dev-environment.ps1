f18ee03e23e9ad5141e51acb05317824c26d6345
Ivaylo <ivaylolivanov95@gmail.com>
2023-10-13 02:39:11 +0300

    Add dev-environment.ps1
    Signed-off-by: Ivaylo <ivaylolivanov95@gmail.com>

---
+++ tools/windows/dev-environment.ps1
@@ -0,0 +1,7 @@
+# This file houses the environmental variables, specific to
+# the developer's user. Replace the placeholder with relevant value.
+#
+# * Will be "sourced" by the prepare.bat script.
+
+# Set to the root of the project
+$global:WorkingDirRoot="D:\Projects\tanks";
