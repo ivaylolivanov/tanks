55551db378c509f7232b82469ed7ace81166e5a9
Ivaylo Ivanov <ivaylolivanov95@gmail.com>
2024-10-14 23:14:01 +0300

    math.h: Add CeilReal32ToInt32()
    Signed-off-by: Ivaylo Ivanov <ivaylolivanov95@gmail.com>

--- src/math/math.h
+++ src/math/math.h
@@ -1,4 +1,5 @@
 #if !defined MATH
+#include <cmath>
 
 inline void ClampInt32(int32& value, int32 min, int32 max)
 {
@@ -22,6 +23,15 @@ inline void ClampReal32(real32& value, real32 min, real32 max)
         value = min;
     if (value > max)
         value = max;
+}
+
+inline int32 CeilReal32ToInt32(real32 value)
+{
+    int32 result = 0;
+
+    result = (int32)ceilf(value);
+
+    return result;
 }
 
 #define MATH
