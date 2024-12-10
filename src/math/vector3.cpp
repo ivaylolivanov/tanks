1e6f6dc77ea1cefdd39616c24c43ca3f7025923f
Ivaylo Ivanov <ivaylolivanov95@gmail.com>
2024-12-10 12:12:38 +0200

    vector3: Fix operator overload & add real32 * overload
    Signed-off-by: Ivaylo Ivanov <ivaylolivanov95@gmail.com>

--- src/math/vector3.cpp
+++ src/math/vector3.cpp
@@ -42,15 +42,32 @@ inline V3r operator*(V3r a, int32 b)
     return result;
 }
 
-inline V3r &operator*=(V3r &a, int32 b)
-{
-    a = a * b;
-    return a;
-}
-
 inline V3r operator*(int32 a, V3r b)
 {
     V3r result;
     result = b * a;
     return result;
+}
+
+inline V3r operator*(V3r a, real32 b)
+{
+    V3r result;
+    result.X = a.X * b;
+    result.Y = a.Y * b;
+    result.Z = a.Z * b;
+
+    return result;
+}
+
+inline V3r operator*(real32 a, V3r b)
+{
+    V3r result;
+    result = b * a;
+    return result;
+}
+
+inline V3r &operator*=(V3r &a, int32 b)
+{
+    a = a * b;
+    return a;
 }
